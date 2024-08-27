1a9419732fa90c973383675018909a44910816c8
HeisenBerg? <admin@tiktokastudio.com>
2024-08-27 11:29:08 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -70,9 +70,6 @@ async function fetchRepoData() {
 
     try {
         const response = await fetch(`/analyze?repo=${encodeURIComponent(repoUrl)}&apiKey=${encodeURIComponent(apiKey)}&model=${encodeURIComponent(selectedModel)}`);
-        url='https://eatrepo.v2ray-tokyo.workers.dev'
-        url='https://repototextforllms.pages.dev'
-        //const response = await fetch(`${url}/analyze?repo=${encodeURIComponent(repoUrl)}&apiKey=${encodeURIComponent(apiKey)}&model=${encodeURIComponent(selectedModel)}`);
 
         if (!response.ok) {
             throw new Error(`API request failed with status ${response.status}`);
